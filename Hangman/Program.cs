b99fa0351a07a6271e63e27e6472ffb0b3eb002d
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-10-09 16:58:40 -0400

    initial Commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Hangman;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //intro message
+        Console.WriteLine("Hello, Today we will be playing Hangman!");
+        
+        // Populate list with random words ( look into word generator like name generator exists e.g test data)
+        const int NUMBEROFGUESSES = 10;
+        List<string> words = new List<string>();
+        words.Add("Tom");
+        words.Add("populate");
+        words.Add("population");
+        words.Add("Tom");
+        words.Add("Tom");
+        words.Add("Tom");
+        words.Add("Tom");
+        
+        
+        int NumberOfListItems = words.Count;
+        
+        Console.WriteLine(NumberOfListItems);
+        Random rand = new();
+        rand.Next(0, NumberOfListItems);
+        
+        
+        
+        
+        
+    }
+}
