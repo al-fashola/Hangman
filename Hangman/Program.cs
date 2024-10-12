36fe0f4dab21484a4a26a380668bef60572fce95
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-10-11 23:05:08 -0400

    identify where the guessed character is located if it is a part of the selected word

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -12,14 +12,17 @@ class Program
         const int MAXWRONGGUESSES = 3;
         
         List<string> words = new List<string>();
+        words.Add("archaebacteria");
+        /*words.Add("cataclysmic");
         words.Add("liposuction");
-        words.Add("populate");
+        words.Add("extravaganza");
         words.Add("population");
         words.Add("Tomorrow");
         words.Add("optimization");
         words.Add("calibration");
         words.Add("aptitude");
         words.Add("conceited");
+        */
         
         int numberOfListItems = words.Count;
         
@@ -32,17 +35,41 @@ class Program
         int numberOfGuesses = 0;
         int wrongGuesses = 0;
         
+        string guessesString = String.Concat(Enumerable.Repeat("_", word.Length));
+        Console.WriteLine(guessesString);
         
-/*
         while (numberOfGuesses <= NUMBEROFGUESSES & wrongGuesses < MAXWRONGGUESSES)
         {
             Console.WriteLine("Guess a letter:");
             string guess = Console.ReadLine();
-            guess.ToCharArray()
+
+            List<int> charIndex = [];
+            int startAt = 0;
+            int charLocation = 0;
+            int charCount = 0;
+
+            //identify if the guessed character is part of the word and where it is located 
+            while (charLocation != -1)
+            {
+                charLocation = word.IndexOf(guess, startAt);
+                if (charCount == 0 & charLocation == -1)
+                {
+                    wrongGuesses++;
+                    break;
+                }
+                charIndex.Add(charLocation);
+                startAt = charLocation + 1;
+                charCount++;
+            }
+            // insert guessed char at indexes provided
+            
+            
+            
+           // guess.ToCharArray()
 
 
         }
-        */
+        
         //string x = ["_"] * NumberOfListItems; 
         
         //List<string> hangmanWord = new List<string>();
