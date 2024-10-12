52f0b7522f7af66d155818dae327ee020dcac25d
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-10-11 22:07:33 -0400

    Filled list + selecting random entry

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -9,24 +9,51 @@ class Program
         
         // Populate list with random words ( look into word generator like name generator exists e.g test data)
         const int NUMBEROFGUESSES = 10;
+        const int MAXWRONGGUESSES = 3;
+        
         List<string> words = new List<string>();
-        words.Add("Tom");
+        words.Add("liposuction");
         words.Add("populate");
         words.Add("population");
-        words.Add("Tom");
-        words.Add("Tom");
-        words.Add("Tom");
-        words.Add("Tom");
+        words.Add("Tomorrow");
+        words.Add("optimization");
+        words.Add("calibration");
+        words.Add("aptitude");
+        words.Add("conceited");
         
-        int NumberOfListItems = words.Count;
+        int numberOfListItems = words.Count;
         
-        Console.WriteLine(NumberOfListItems);
+        //Console.WriteLine(NumberOfListItems);
         Random rand = new();
-        rand.Next(0, NumberOfListItems);
+        int guessListInteger = rand.Next(0, numberOfListItems);
+        string word = words[guessListInteger];
+        word = word.ToUpper();
+        
+        int numberOfGuesses = 0;
+        int wrongGuesses = 0;
         
         
+/*
+        while (numberOfGuesses <= NUMBEROFGUESSES & wrongGuesses < MAXWRONGGUESSES)
+        {
+            Console.WriteLine("Guess a letter:");
+            string guess = Console.ReadLine();
+            guess.ToCharArray()
+
+
+        }
+        */
+        //string x = ["_"] * NumberOfListItems; 
         
+        //List<string> hangmanWord = new List<string>();
+        Console.WriteLine($"Guessed word: {word}");
         
-        
+        Console.WriteLine($"The word is : {word} and guess integer is: {guessListInteger}");
+
+        Console.WriteLine(word.IndexOf('O'));
+
+
+
+
     }
 }
